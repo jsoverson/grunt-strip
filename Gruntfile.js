4946105247142a5ac81b656d17ad1aaba10483f3
Jarrod Overson <joverson@onerecovery.com>
2012-09-23 23:01:56 -0700

    initial commit, pulling out of other project

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    test: {
+      files: ['test/**/*test.js']
+    },
+    lint: {
+      files: ['Gruntfile.js', 'tasks/**/*.js', 'test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'default'
+    },
+    strip : {
+      main : {
+        //files : 'test/fixtures/src/all_api_methods.built.js',
+        //inline : true,
+        src : 'test/fixtures/src/all_api_methods.js',
+        dest : 'test/fixtures/src/all_api_methods.built.js',
+        nodes : ['iog','console']
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        es5: true
+      },
+      globals: {}
+    }
+  });
+
+  // Load local tasks.
+  grunt.loadTasks('tasks');
+
+  // Default task.
+  grunt.registerTask('default', 'lint test');
+
+};
